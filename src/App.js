d198956456ba2eb93b865227839bfe42d2692a2f
Rahul NR <rahulnr@Rahuls-MacBook-Air.local>
2021-10-10 15:29:33 +0530

    sever side rendering app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useEffect, useState } from "react";
+import styled from "styled-components";
+
+import { Link, Route, Switch } from "react-router-dom";
+const Home = React.lazy(() => import("./Home"));
+const About = React.lazy(() => import("./About"));
+const Contact = React.lazy(() => import("./Contact"));
+
+const MyLink = styled.li`
+  > a {
+    color: blue;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>My server side rendering app</h1>
+      <ul>
+        <MyLink>
+          <Link to="/home">Home</Link>
+        </MyLink>
+        <MyLink>
+          <Link to="/about">About</Link>
+        </MyLink>
+        <MyLink>
+          <Link to="/contact">Contact</Link>
+        </MyLink>
+      </ul>
+      <Switch>
+        <SuspenseHandler>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+        </SuspenseHandler>
+      </Switch>
+    </>
   );
+}
+
+function SuspenseHandler(props) {
+  const [state, setState] = useState(false);
+  useEffect(() => {
+    if (window && window.preloadedData) {
+      setState(true);
+    }
+  });
+  if (state)
+    return (
+      <Suspense fallback={<div>loading....</div>}>{props.children}</Suspense>
+    );
+  return <div>Loading</div>;
 }
 
 export default App;
